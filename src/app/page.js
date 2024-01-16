352cbc3bd11b583fbfbe7cd63da8fb0f141719ef
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-16 16:52:19 +0600

    install daisayUI and add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/Components/Shared/Footer";
 import Navbar from "@/Components/Shared/Navbar";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <Navbar />
       Home Page
+      <Footer></Footer>
     </div>
   )
 }
