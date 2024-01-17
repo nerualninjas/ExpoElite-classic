48d15761f2a5cd4554e98e9171d842e55eaced6d
husneara <husnearaa99@gmail.com>
2024-01-18 03:28:09 +0600

    changed the section position

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import ExploreProperty from '@/Components/HomePage/ExploreByLocation/ExploreProperty';
-import PropertyCard from '@/Components/HomePage/ExploreByLocation/PropertyCard';
 import FAQ from '@/Components/HomePage/FQA/FAQ';
 import Featured from '@/Components/HomePage/Featured';
 import Count from '@/Components/HomePage/Statistics/Count';
@@ -9,11 +8,12 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
+            
             <Featured />
             <Count />
             <Review />
-            <FAQ />
             <ExploreProperty />
+            <FAQ />
         </div>
     );
 };
