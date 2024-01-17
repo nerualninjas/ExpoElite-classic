10ba829e946504e8c4ce830d02a11194f03654fa
sabrinara <you@example.com>
2024-01-17 19:23:04 +0600

    FAQ section

---
+++ src/Components/HomePage/FQA/FAQ.jsx
@@ -0,0 +1,52 @@
+'use client'
+import Lottie from "lottie-react";
+import Ex from "./FAQ.json";
+import AccordianItems from "./AccordianItems";
+import { useState } from "react";
+const FAQ = () => {
+    const [open, setOpen] = useState(false);
+
+    const toggle = (index) => {
+        if (open === index) {
+            return setOpen(null);
+        }
+
+        setOpen(index);
+    }
+    const accourdianData = [
+        {
+            title : "What is Expo Elite ?",
+            desc  : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. ",
+        },
+        {
+            title : "What is Expo Elite ?",
+            desc  : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. ",
+        },
+        {
+            title : "What is Expo Elite ?",
+            desc  : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. ",
+        },
+        {
+            title : "What is Expo Elite ?",
+            desc  : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, tenetur. ",
+        }
+    ]
+    return (
+        <div className="flex flex-col md:flex-row items-center justify-center mx-4 py-1 px-4 rounded">
+            <div className="w-1/3 md:w-1/2 mt-16 md:mt-1">
+                <Lottie animationData={Ex} loop={true} />
+            </div>
+            <div className="w-2/3 md:w-1/2 mt-16 md:mt-1">
+                <section className="bg-[#0c7e5c] rounded py-4">
+                    <div className="text-white text-xl px-8">
+                        {accourdianData.map((item, index) => (
+                            <AccordianItems open={open === index} toggle={() => toggle(index)} key={index} title={item.title} desc={item.desc} />
+                        ))}
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+};
+
+export default FAQ;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
+import FAQ from '@/Components/HomePage/FQA/FAQ';
 import Featured from '@/Components/HomePage/Featured';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div>
-            <Featured />
 
+            <Featured />
+            <FAQ/>
         </div>
     );
 };
